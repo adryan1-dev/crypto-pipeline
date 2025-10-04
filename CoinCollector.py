bbb0421405dc7a603d3b4fcff4df7a44deafc423
Adryan <130511954+adryan1-dev@users.noreply.github.com>
2025-10-04 17:37:02 -0300

    Update CoinCollector.py
    security updates

--- CoinCollector.py
+++ CoinCollector.py
@@ -4,7 +4,7 @@ from typing import Dict,List, Any
 from BasePipeline import BaseCollector
 class CoinCollector(BaseCollector):
     API_URL = "https://api.coingecko.com/api/v3/coins/markets"
-    API_KEY = "CG-Z8TRYAG3c8Fva7bLwJ9U4piQ"
+    API_KEY = "xxxxxxxxxxx"
 
     def fetch_data(self) -> List[Dict[str,Any]]:
         print(f'-> Coletando dados da CoinGecko para {self._fiat_coin.upper()}...')
